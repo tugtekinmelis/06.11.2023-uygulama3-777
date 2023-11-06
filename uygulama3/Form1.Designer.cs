36ecbb6b6dd999055bb081bcbe019c16574031d7
tugtekinmelis <145451246+tugtekinmelis@users.noreply.github.com>
2023-11-06 12:02:06 +0300

    Add files via upload

---
+++ uygulama3/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace uygulama3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnSayi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(269, 79);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(57, 20);
+            this.lblSonuc.TabIndex = 0;
+            this.lblSonuc.Text = "label1";
+            // 
+            // btnSayi
+            // 
+            this.btnSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSayi.Location = new System.Drawing.Point(30, 54);
+            this.btnSayi.Name = "btnSayi";
+            this.btnSayi.Size = new System.Drawing.Size(191, 70);
+            this.btnSayi.TabIndex = 1;
+            this.btnSayi.Text = "Rastgele Sayı Oluştur";
+            this.btnSayi.UseVisualStyleBackColor = true;
+            this.btnSayi.Click += new System.EventHandler(this.btnSayi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 169);
+            this.Controls.Add(this.btnSayi);
+            this.Controls.Add(this.lblSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnSayi;
+    }
+}
+
---
+++ uygulama3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayi_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sayi = random.Next(0, 101);  
+
+            lblSonuc.Text = sayi.ToString();
+
+            if (sayi < 50)
+            {
+                lblSonuc.ForeColor = Color.Red;
+            }
+            else if (sayi >=50 && sayi <=70)
+            {
+                lblSonuc.ForeColor = Color.Orange;
+            }
+            else
+            {
+                lblSonuc.ForeColor = Color.Blue;
+            }
+        }
+    }
+}
